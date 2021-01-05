85b4c00d1f83f70d1387967ee83d8cc493f5f4bc
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2021-01-05 21:52:04 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# domManipulationBootstrap
+"# domManipulationBootstrap" 
---
+++ app.js
@@ -0,0 +1,13 @@
+const a =document.getElementById('avalinjomle');
+a.innerHTML = "dom ro taghir dadam"
+
+
+
+// function myFunction(inserttrxt) {
+//     document.getElementById('inserttext').innerHTML = "Hello mani";
+//   // }
+//   // function myFunction() {
+//   //    const a = document.querySelectorAll('inserttext').innerHTML="dige ali shod";
+//   //    }
+//   const a =document.getElementById("password").innerHTML
+//   console.log(a);

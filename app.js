759535c85ce40d7c1ecd5329d9e24da66f109004
ManiMovassagh <mani.mgh@gmail.com>
2021-01-05 20:21:28 +0100

    this is my third commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const a =document.getElementById("password").innerHTML
+console.log(a);
+
+
+
+// function myFunction(inserttrxt) {
+//     document.getElementById('inserttext').innerHTML = "Hello mani";
+//   // }
+//   // function myFunction() {
+//   //    const a = document.querySelectorAll('inserttext').innerHTML="dige ali shod";
+//   //    }
+//   const a =document.getElementById("password").innerHTML
+//   console.log(a);
